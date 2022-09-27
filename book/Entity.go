9e72bcf7b07faca7226bd5429a037b6ecb7264f5
MuhamadSholikhudin <muhammadsholihudin18@gmail.com>
2022-09-27 14:21:15 +0700

    2022 09 27 14:21

--- book/Entity.go
+++ book/Entity.go
@@ -3,11 +3,13 @@ package book
 import "time"
 
 type Book struct {
-	ID         int
-	Title      string
-	Descrition string
-	Price      int
-	Rating     int
-	CreatedAt  time.Time
-	UpdatedAt  time.Time
+	ID          int
+	Title       string
+	Descrition  string
+	Price       int
+	Rating      int
+	Discount    int
+	Description string
+	CreatedAt   time.Time
+	UpdatedAt   time.Time
 }
--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"log"
+	"pustaka-api/book"
 	"pustaka-api/handler"
 
 	"github.com/gin-gonic/gin"
@@ -36,12 +37,57 @@ func main() {
 
 	//Connection DB MYSQL
 	dsn := "root:@tcp(127.0.0.1:3306)/pustaka-api?charset=utf8mb4&parseTime=True&loc=Local"
-	_, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 	if err != nil {
 		log.Fatal("DB Connectin Error")
 
 	}
 	fmt.Println("Database Connected")
+
+	// Auto migration
+	// db.AutoMigrate(&book.Book{})
+
+	//CRUD
+
+	///Create
+	// book := book.Book{}
+	// book.Title = "Man Tiger"
+	// book.Price = 90000
+	// book.Rating = 101
+	// book.Description = "Ini adalah Film Man Tiger"
+
+	// err = db.Create(&book).Error
+	// if err != nil {
+	// 	fmt.Println("Error Creating book")
+	// }
+
+	// Read
+
+	// var book book.Book
+
+	// // err = db.Debug().First(&book).Error // Get By Last
+	// err = db.Debug().First(&book, 1).Error // Get By id
+	// if err != nil {
+	// 	fmt.Println("Error Read First book")
+	// }
+
+	// fmt.Println("Output Title : ", book.Title)
+	// fmt.Println("Output Price : ", book.Price)
+	// fmt.Println("Output Rating : ", book.Rating)
+	// fmt.Println("Output Description", book.Description)
+
+	var books []book.Book
+
+	// err = db.Debug().Find(&books, 1).Error // GET data ROWS
+	err = db.Debug().Where("title = ?", "Man Tiger").Find(&books).Error // GET data ROWS WHERE
+	if err != nil {
+		fmt.Println("Error Read First book")
+	}
+
+	for _, b := range books {
+		fmt.Println("Output Book : ", b.Title)
+
+	}
 
 	//Versioning API
 	v1 := gin.Default()
